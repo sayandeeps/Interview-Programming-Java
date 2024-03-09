8ef383fbead64a04ed159fb28ea1c58a9bd207e2
Sayandeep <sayandeep11@gmail.com>
2024-03-09 15:12:19 +0530

    fixed

--- 1_WAP_to_check_A_character_is_a_vowel_or_consonant.java
+++ 1_WAP_to_check_A_character_is_a_vowel_or_consonant.java
@@ -1,10 +1,25 @@
+//1. WAP to check A character is a vowel or consonant
+
+import java.util.Scanner;
+
 class A {
-    public static void main(String[] args) {
-        char ch = 'a';
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+    void checkVowelOrConsonant(char ch) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
+                || ch == 'O' || ch == 'U') {
             System.out.println("Vowel");
-        } else {
+        } else if (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z') {
             System.out.println("Consonant");
+        } else if (ch >= '0' && ch <= '9') {
+            System.out.println("Number");
+        } else {
+            System.out.println("Special Character");
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        A obj = new A();
+        obj.checkVowelOrConsonant(ch);
     }
 }
